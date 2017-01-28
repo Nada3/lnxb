37d1003fde83336e39d87db66f039afc49cb261b
Nada's Strength <noltheo03@gmail.com>
2017-01-28 22:05:33 +0000

    Update config.py

--- config.py
+++ config.py
@@ -3,19 +3,20 @@ __VERSION__ = '0.1'
 __FULLNAME__ = '%s-%s' % (__NAME__, __VERSION__)
 
 REQUIRED_FILES = (
-	'/etc/%s/',
-	'/etc/%s/rules.conf',
+	'/etc/%s/' % __FULLNAME__,
+	'/etc/%s/rules.conf' % __FULLNAME__,
 )
 
 DEFAULT_CONFIGURATION = (
-	('settings'),
-	('settings', 'SetBackupFolder', '/etc/%s/backups/'),
+	('settings', 0),
+	('settings', 'SetBackupFolder', '/etc/%s/backups/' % __FULLNAME__),
 	('settings', 'SetBackupTimeout', '60'),
+	('settings', 'SetBackupDatetime', '00h00'),
 	('settings', 'DataOverwriting', 'true'),
 	('settings', 'DataCompression', 'false'),
 	('settings', 'DataCompressionType', 'plain'),
 
-	('backup_0'),
+	('backup_0', 0),
 	('backup_0', 'BackupId', '0'),
 	('backup_0', 'BackupPath', '/home/'),
 	('backup_0', 'BackupType', 'folder'),
@@ -24,7 +25,7 @@ DEFAULT_CONFIGURATION = (
 
 INITIALISATION_CONSTANTS = {
 	'DEFAULT_CONFIGURATION': DEFAULT_CONFIGURATION,
-	'INSTALLATION_LOG': '/etc/%s/install.log',
-	'FOLDERS': ('/etc/%s/', '/etc/%s/backups/'),
-	'RULES': '/etc/%s/rules.conf',
+	'INSTALLATION_LOG': '/etc/%s/install.log' % __FULLNAME__,
+	'FOLDERS': ('/etc/%s/' % __FULLNAME__, '/etc/%s/backups/' % __FULLNAME__),
+	'RULES': '/etc/%s/rules.conf' % __FULLNAME__,
 }
